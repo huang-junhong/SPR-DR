bfca9c600ebff8aa8bb0611c7f4cfbabf2c1fb4a
huang-junhong <xx1217522411@outlook.com>
2025-07-17 10:42:58 +0800

    Add files via upload

--- torch_model/__init__.py
+++ torch_model/__init__.py
@@ -2,11 +2,11 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+from SwinIR import SwinIR
+from SRRes import SRRes_SRF2, SRRes
+from RRDN import RRDBNet_SRF2, RRDBNet
+from Real_ESRGAN import RRDBNet as Real_RRDBNet
 
 from Stander_Discriminator import Stander_Discriminator
 from Stander_Discriminator_dynamic_dropout import Stander_Discriminator_with_dynamic_dropout
-
-
-__all__ = [Stander_Discriminator,
-           Stander_Discriminator_with_dynamic_dropout]
 
